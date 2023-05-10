c005f2b7b39a639cf7955b6fe90e8d8430f18b9d
Tristan Opp <tristanopp@MacBook-Pro-de-Tristan.local>
2023-05-10 16:06:03 +0200

    mail_envoi

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\Controller as Mailcontrol;
+use App\Mail\OrderShipped;
+use App\Models\Order;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+}
+
+class OrderShipmentController extends Mailcontrol
+{
+    /**
+     * Ship the given order.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $order = Order::findOrFail($request->order_id);
+ 
+        // Ship the order...
+ 
+        Mail::to($request->user())->send(new OrderShipped($order));
+ 
+        return redirect('/orders');
+    }
 }
--- config/services.php
+++ config/services.php
@@ -17,8 +17,9 @@ return [
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        /*'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
         'scheme' => 'https',
+        */
     ],
 
     'postmark' => [
